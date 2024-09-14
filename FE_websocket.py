8867f45a359456c5b501b6b43a457af819f630e7
Syed Muhammad Zaki <szaki1871993@gmail.com>
2024-09-15 00:04:50 +0500

    update 1

--- FE_websocket.py
+++ FE_websocket.py
@@ -4,7 +4,8 @@ import websockets
 
 # WebSocket connection logic
 async def send_message(message):
-    uri = "ws:https://e980-154-192-145-13.ngrok-free.app:8183"
+    # Replace with your ngrok HTTPS URL
+    uri = "wss://9952-154-192-145-13.ngrok-free.app"
     async with websockets.connect(uri) as websocket:
         await websocket.send(message)
 
